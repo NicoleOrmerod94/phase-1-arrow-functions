bf8488730843e8b40e3ccf5ec43acd53047d9108
NicoleOrmerod94 <147579584+NicoleOrmerod94@users.noreply.github.com>
2024-05-09 13:30:18 -0600

    arrow funtions

---
+++ index.js
@@ -0,0 +1,26 @@
+const divide = (a, b) => {
+    if (b === 0) [
+    ]
+    return a / b;
+//Export the divide function using an arrow array
+module.exports = {
+    divide
+}}
+
+const square = (X) => { X * X;
+    return X * X
+}
+
+//Export the square function using an array
+module.exports = {
+    square
+}
+
+function add (a, b) { return a + b; }; //added return statement to close braces and solve syntax error.
+    let a = 3
+    let b = 4
+// export the add function using an array
+    //no empty line below.
+module.exports = {
+    add
+}
